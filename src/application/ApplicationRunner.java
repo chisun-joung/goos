4b30cdd7501fc26147f5bda76ad0134fb658228b
chisun-joung <csjoung@gmail.com>
2013-01-31 09:56:06 +0900

    Make AutionSniperEndToEndTest

---
+++ src/application/ApplicationRunner.java
@@ -0,0 +1,22 @@
+package application;
+
+import fake.FakeAuctionServer;
+
+public class ApplicationRunner {
+
+	public void startBiddingIn(FakeAuctionServer auction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showsSniperHasLostAuction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ tests/endtoendtest/AuctionSniperEndToEndTest.java
@@ -0,0 +1,34 @@
+package endtoendtest;
+
+import org.junit.After;
+import org.junit.Test;
+
+import application.ApplicationRunner;
+
+import fake.FakeAuctionServer;
+
+public class AuctionSniperEndToEndTest {
+	private final FakeAuctionServer auction = new FakeAuctionServer(
+			"item-54321");
+	private final ApplicationRunner application = new ApplicationRunner();
+
+	@Test
+	public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+		auction.startSellingItem(); // Step 1
+		application.startBiddingIn(auction); // Step 2
+		auction.hasReceivedJoinRequestFromSniper(); // Step 3
+		auction.announceClosed(); // Step 4
+		application.showsSniperHasLostAuction(); // Step 5
+	}
+
+	// Additional cleanup
+	@After
+	public void stopAuction() {
+		auction.stop();
+	}
+
+	@After
+	public void stopApplication() {
+		application.stop();
+	}
+}
