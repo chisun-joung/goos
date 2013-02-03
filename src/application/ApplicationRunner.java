2b08b3398ffc0603a2427111f80880faeda98088
chisun-joung <csjoung@gmail.com>
2013-02-04 03:41:14 +0900

    ApplicationRunner AuctionSniprerDRiver

--- src/application/ApplicationRunner.java
+++ src/application/ApplicationRunner.java
@@ -3,20 +3,29 @@ package application;
 import fake.FakeAuctionServer;
 
 public class ApplicationRunner {
-
-	public void startBiddingIn(FakeAuctionServer auction) {
-		// TODO Auto-generated method stub
-		
-	}
+	public static final String SNIPER_ID = "sniper";
+	public static final String SNIPER_PASSWORD = "sniper";
+	private AuctionSniperDriver driver;
+	public void startBiddingIn(final FakeAuctionServer auction) { 
+		Thread thread = new Thread("Test Application") {
+	
+	@Override public void run() {  try {
+	Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId()); } catch (Exception e) {
+	e.printStackTrace();  }
+	} };
+	thread.setDaemon(true);
+	thread.start();
+	driver = new AuctionSniperDriver(1000); 
+	driver.showsSniperStatus(STATUS_JOINING); 
 
 	public void showsSniperHasLostAuction() {
-		// TODO Auto-generated method stub
+		driver.showsSniperStatus(STATUS_LOST);
 		
 	}
 
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		if (driver != null) { driver.dispose(); 
+		}
 	}
 
 }
