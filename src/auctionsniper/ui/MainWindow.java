8f2cfe46d673a7f9929c278e914fdfe903aa7539
chisun-joung <csjoung@gmail.com>
2013-02-05 06:42:37 +0900

    I finally finish chapther 11 Wuh~

--- src/auctionsniper/ui/MainWindow.java
+++ src/auctionsniper/ui/MainWindow.java
@@ -2,7 +2,7 @@ package auctionsniper.ui;
 
 import static auctionsniper.Main.MAIN_WINDOW_NAME;
 import static auctionsniper.Main.SNIPER_STATUS_NAME;
-import static endtoendtest.ApplicationRunner.STATUS_JOINING;
+
 import java.awt.Color;
 
 import javax.swing.JFrame;
@@ -11,7 +11,10 @@ import javax.swing.border.LineBorder;
 
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame {
+	public static final String STATUS_LOST = "Lost";
+	private static final String STATUS_JOINING = "Joining";
 	private final JLabel sniperStatus = createLabel(STATUS_JOINING);
+
 	public MainWindow() {
 		super("Auction Sniper");
 		setName(MAIN_WINDOW_NAME);
@@ -20,11 +23,15 @@ public class MainWindow extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	
-	private static JLabel createLabel(String initialText) { 
-		JLabel result = new JLabel(initialText); 
-		result.setName(SNIPER_STATUS_NAME); 
-		result.setBorder(new LineBorder(Color.BLACK)); 
+
+	private static JLabel createLabel(String initialText) {
+		JLabel result = new JLabel(initialText);
+		result.setName(SNIPER_STATUS_NAME);
+		result.setBorder(new LineBorder(Color.BLACK));
 		return result;
+	}
+
+	public void showStatus(String status) {
+		sniperStatus.setText(status);
 	}
 }
