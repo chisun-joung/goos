8fed45af8e33788dab8306f3e433d03c3f0a5457
chisun-joung <csjoung@gmail.com>
2013-02-21 17:17:15 +0900

    0221_1

--- tests/endtoendtest/AuctionSniperDriver.java
+++ tests/endtoendtest/AuctionSniperDriver.java
@@ -9,12 +9,14 @@ import com.objogate.wl.swing.driver.JLabelDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 public class AuctionSniperDriver extends JFrameDriver {
+	@SuppressWarnings("unchecked")
 	public AuctionSniperDriver(int timeoutMillis) {
 		super(new GesturePerformer(), JFrameDriver.topLevelFrame(
 				named(Main.MAIN_WINDOW_NAME), showingOnScreen()),
 				new AWTEventQueueProber(timeoutMillis, 100));
 	}
 
+	@SuppressWarnings("unchecked")
 	public void showsSniperStatus(String statusText) {
 		new JLabelDriver(this, named(Main.SNIPER_STATUS_NAME))
 				.hasText(equalTo(statusText));
