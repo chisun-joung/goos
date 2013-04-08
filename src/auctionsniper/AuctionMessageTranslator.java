46211f062995ec63d046422e76ff2d52d76f5177
chisun-joung <csjoung@gmail.com>
2013-04-08 23:48:02 +0900

    first test

--- src/auctionsniper/AuctionMessageTranslator.java
+++ src/auctionsniper/AuctionMessageTranslator.java
@@ -6,10 +6,16 @@ import org.jivesoftware.smack.packet.Message;
 
 public class AuctionMessageTranslator implements MessageListener{
 
+	private AuctionEventListener listener;
+
+	public AuctionMessageTranslator(AuctionEventListener listener) {
+		this.listener = listener;
+	}
+
 	@Override
 	public void processMessage(Chat chat, Message message) {
 		
-		
+		listener.auctionClosed();
 	}
 
 }
